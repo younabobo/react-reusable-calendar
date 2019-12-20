ef85a7f2729bb03740eb23f9275dc1bf87ecaa6f
Younes Henni <36455366+younabobo@users.noreply.github.com>
2019-12-20 09:47:19 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    // "jest/globals": true
+  },
+  extends: [
+    'plugin:react/all',
+    'airbnb',
+    "plugin:jest/all",
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react', 'jest'
+  ],
+  rules: {
+  },
+};
